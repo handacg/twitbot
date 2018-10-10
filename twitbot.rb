c0d02b7c6299a503140e892960478a7ed12bd513
handacg <handa.cheng@gmail.com>
2018-10-10 11:45:36 +0200

    ENV error fixed

--- twitbot.rb
+++ twitbot.rb
@@ -1,17 +1,16 @@
 require 'twitter'
 require 'dotenv'
-dotenv.load
+
+Dotenv.load
 
 # quelques lignes qui enregistrent les clés d'APIs
-client = Twitter::Streaming::Client.new do |config|
- config.consumer_key        = env["TWITTER_API_KEY"]
-  config.consumer_secret     = env["TWITTER_API_SECRET"]
-  config.access_token        = env["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = env["TWITTER.ACCESS_TOKEN_SECRET"]
+client = Twitter::REST::Client.new do |config|
+ config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER.ACCESS_TOKEN_SECRET"]
 end
 p client
 
-topics = ["coffee", "tea"]
-client.filter(track: topics.join(",")) do |object|
-  puts object.text if object.is_a?(Twitter::Tweet)
-end
+client.update("hello world")
+
